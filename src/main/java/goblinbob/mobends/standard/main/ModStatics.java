e6eb4aeff1ee077e36b2b8c2dff4ea3aaaa38a79
OutlinedArc217 <67988913+OutlinedArc217@users.noreply.github.com>
2025-07-30 15:53:09 +0800

    Update ModStatics.java

--- src/main/java/goblinbob/mobends/standard/main/ModStatics.java
+++ src/main/java/goblinbob/mobends/standard/main/ModStatics.java
@@ -4,8 +4,12 @@ public class ModStatics
 {
     public static final String MODID = "mobends";
     public static final String LOADER = "forge";
-    public static final String VERSION = "1.1.0";
-    public static final String MCVERSION = "1.12.2";
-
+    public static final String VERSION = "2.0.0";
+    public static final String MCVERSION = "1.20.1";
+    
+    public static final String PROTOCOL_VERSION = "1";
+    
     public static final String VERSION_STRING = VERSION + "/" + MCVERSION + "-" + LOADER;
+    
+    public static final String RESOURCE_PREFIX = MODID + ":";
 }
