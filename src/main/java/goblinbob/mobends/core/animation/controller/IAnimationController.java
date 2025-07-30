1f3c3a03295d6b90e8438a3d58825a7e61fa663e
OutlinedArc217 <67988913+OutlinedArc217@users.noreply.github.com>
2025-07-30 16:34:54 +0800

    Update IAnimationController.java

--- src/main/java/goblinbob/mobends/core/animation/controller/IAnimationController.java
+++ src/main/java/goblinbob/mobends/core/animation/controller/IAnimationController.java
@@ -1,21 +1,23 @@
-package goblinbob.mobends.core.animation.controller;
+package goblinbob.mobends.core.animation;
 
-import goblinbob.mobends.core.data.EntityData;
+import goblinbob.mobends.core.animation.state.AnimationState;
+import goblinbob.mobends.core.math.Transform;
+import net.minecraft.world.entity.Entity;
 
-import javax.annotation.Nullable;
-import java.util.Collection;
+import java.util.Optional;
 
-/*
- * This interface is responsible for updating animation for each
- * instance of an AnimatedEntity.
- * 
- * It's a member of an EntityData instance, and it holds all
- * the information about the current animation's state.
- * */
-public interface IAnimationController<T extends EntityData<?>>
-{
-
-	@Nullable
-	Collection<String> perform(T entityData);
-	
+public interface IAnimationController {
+    void update(Entity entity, float partialTicks);
+    Optional<AnimationState> getState(String name);
+    void setState(String name, AnimationState state);
+    void removeState(String name);
+    Transform getGlobalTransform();
+    
+    static boolean getGlobalAnimationsEnabled() {
+        return AnimationController.getGlobalAnimationsEnabled();
+    }
+    
+    static void setGlobalAnimationsEnabled(boolean enabled) {
+        AnimationController.setGlobalAnimationsEnabled(enabled);
+    }
 }
