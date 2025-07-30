f91a8fdd2f34bbedbc3620b9f200e86b90b84d96
OutlinedArc217 <67988913+OutlinedArc217@users.noreply.github.com>
2025-07-30 16:12:46 +0800

    Update IModule.java

--- src/main/java/goblinbob/mobends/core/module/IModule.java
+++ src/main/java/goblinbob/mobends/core/module/IModule.java
@@ -1,9 +1,22 @@
 package goblinbob.mobends.core.module;
 
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
-public interface IModule
-{
-    void preInit(FMLPreInitializationEvent event);
-    void onRefresh();
+public interface IModule {
+    default void initialize() {}
+    
+    default CompletableFuture<Void> loadResources(Executor executor) {
+        return CompletableFuture.completedFuture(null);
+    }
+
+    default void onRefresh() {}
+    
+    default void onLoadComplete(FMLLoadCompleteEvent event) {}
+    
+    interface Factory {
+        IModule create();
+    }
 }
