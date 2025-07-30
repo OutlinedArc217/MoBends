168fc2fc533ab00139dc1863a17a3aaece47d429
OutlinedArc217 <67988913+OutlinedArc217@users.noreply.github.com>
2025-07-30 16:04:15 +0800

    Update CoreClient.java

--- src/main/java/goblinbob/mobends/core/CoreClient.java
+++ src/main/java/goblinbob/mobends/core/CoreClient.java
@@ -10,49 +10,43 @@ import goblinbob.mobends.core.env.EnvironmentModule;
 import goblinbob.mobends.core.supporters.SupporterContent;
 import goblinbob.mobends.core.pack.PackManager;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.resources.IReloadableResourceManager;
+import net.minecraft.server.packs.resources.ReloadableResourceManager;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 import javax.annotation.Nullable;
 
-public class CoreClient extends Core<CoreClientConfig>
-{
+@OnlyIn(Dist.CLIENT)
+public class CoreClient extends Core<CoreClientConfig> {
     private static CoreClient INSTANCE;
-
     private CoreClientConfig configuration;
 
-    CoreClient()
-    {
+    CoreClient() {
         INSTANCE = this;
-
+        
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        
+        modEventBus.addListener(this::clientSetup);
+    
         registerModule(new EnvironmentModule.Factory());
         registerModule(new ConnectionManager.Factory());
         registerModule(new AssetsModule.Factory());
         registerModule(new SupporterContent.Factory());
     }
 
-    @Override
-    public CoreClientConfig getConfiguration()
-    {
-        return configuration;
+    private void clientSetup(final FMLClientSetupEvent event) {
+        event.enqueueWork(() -> {
+            initializeClient();
+        });
     }
 
-    @Override
-    public void preInit(FMLPreInitializationEvent event)
-    {
-        super.preInit(event);
-
-        configuration = new CoreClientConfig(event.getSuggestedConfigurationFile());
-    }
-
-    @Override
-    public void init(FMLInitializationEvent event)
-    {
-        super.init(event);
-
+    private void initializeClient() {
         PackManager.INSTANCE.initialize(configuration);
         KeyboardHandler.initKeyBindings();
 
@@ -61,23 +55,41 @@ public class CoreClient extends Core<CoreClientConfig>
         MinecraftForge.EVENT_BUS.register(new KeyboardHandler());
         MinecraftForge.EVENT_BUS.register(new FluxHandler());
         MinecraftForge.EVENT_BUS.register(new WorldJoinHandler());
+        
+        Minecraft minecraft = Minecraft.getInstance();
+        if (minecraft.getResourceManager() instanceof ReloadableResourceManager resourceManager) {
+            resourceManager.registerReloadListener(new AssetReloadListener());
+        }
 
-        // Registering a listener to whenever resources have been reloaded.
-        IReloadableResourceManager resourceManager = (IReloadableResourceManager) Minecraft.getMinecraft().getResourceManager();
-        resourceManager.registerReloadListener(new AssetReloadListener());
+        MinecraftForge.EVENT_BUS.addListener(this::onClientTick);
+    }
+
+    private void onClientTick(TickEvent.ClientTickEvent event) {
+        if (event.phase == TickEvent.Phase.END) {
+            updateClient();
+        }
+    }
+
+    private void updateClient() {
     }
 
     @Override
-    public void postInit(FMLPostInitializationEvent event)
-    {
-        super.postInit(event);
+    public void setupConfig() {
+        configuration = new CoreClientConfig();
+        ModLoadingContext.get().registerConfig(net.minecraftforge.fml.config.ModConfig.Type.CLIENT, configuration.getSpec());
+    }
 
+    @Override
+    public CoreClientConfig getConfiguration() {
+        return configuration;
+    }
+
+    public void refreshModules() {
+        super.refreshModules();
         EntityBenderRegistry.instance.applyConfiguration(configuration);
     }
 
     @Nullable
-    public static CoreClient getInstance()
-    {
+    public static CoreClient getInstance() {
         return INSTANCE;
     }
-}
